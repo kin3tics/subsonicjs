3b3f7a50826131cd90d636bccb9b0dca1a4b9948
Jeremy Dunn <jeremydunn@wefueldigital.com>
2019-12-01 16:10:49 -0500

    Initial commit

---
+++ test/subsonic.spec.js
@@ -0,0 +1,96 @@
+/* eslint-disable new-cap */
+
+'use strict';
+
+const testUtils = require('../testUtils');
+const Subsonic = require('../lib/subsonic');
+
+const expect = require('chai').expect;
+
+describe('Subsonic Module', function() {
+  this.timeout(20000);
+
+  describe('config object', () => {
+    it('should only accept either an object or a string', () => {
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          123
+        );
+      }).to.throw(/Host config must either be an object or a string/);
+
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          ['2019-12-12']
+        );
+      }).to.throw(/Host config must either be an object or a string/);
+
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          '2019-12-12'
+        );
+      }).to.not.throw();
+
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          {
+            version: '1.16.1',
+          }
+        );
+      }).to.not.throw();
+    });
+
+    it('should only contain allowed properties', () => {
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          {
+            foo: 'bar',
+            version: '1.16.1',
+          }
+        );
+      }).to.throw(/Host config object may only contain the following:/);
+
+      expect(() => {
+        Subsonic(
+          process.env.USERNAME,
+          testUtils.getUserKey(),
+          process.env.SALT,
+          {
+            protocol: 'http',
+            host: 'foo.subsonic.org',
+            port: 321,
+            timeout: 60,
+            client: 'testClient',
+            format: 'json',
+            version: '1.16.1',
+          }
+        );
+      }).to.not.throw();
+    });
+  });
+
+  //   describe('errors', () => {
+  //     it('Exports errors as types', () => {
+  //       const Subsonic = require('../lib/subsonic');
+  //       expect(
+  //         new Subsonic.errors.SubsonicInvalidRequestError({
+  //           message: 'error',
+  //         }).type
+  //       ).to.equal('SubsonicInvalidRequestError');
+  //     });
+  //   });
+});
---
+++ testUtils/index.js
@@ -0,0 +1,166 @@
+'use strict';
+require('dotenv').config();
+// NOTE: testUtils should be require'd before anything else in each spec file!
+require('mocha');
+// Ensure we are using the 'as promised' libs before any tests are run:
+require('chai').use(require('chai-as-promised'));
+
+const md5 = require('md5');
+
+const utils = (module.exports = {
+  getUserKey: () => {
+    const key = md5(`${process.env.PASSWORD}${process.env.SALT}`);
+    return key;
+  },
+
+  getSpyableSubsonic: () => {
+    // Provide a testable subsonic instance
+    // That is, with mock-requests built in and hookable
+
+    const subsonic = require('../lib/subsonic');
+    const subsonicInstance = subsonic(
+      process.env.USERNAME,
+      utils.getUserKey(),
+      process.env.SALT,
+      process.env.HOST
+    );
+
+    subsonicInstance.REQUESTS = [];
+
+    for (const i in subsonicInstance) {
+      makeInstanceSpyable(subsonicInstance, subsonicInstance[i]);
+    }
+
+    function makeInstanceSpyable(subsonicInstance, thisInstance) {
+      if (thisInstance instanceof subsonic.SubsonicResource) {
+        patchRequest(subsonicInstance, thisInstance);
+      }
+    }
+
+    function patchRequest(subsonicInstance, instance) {
+      instance._request = function(method, host, url, data, auth, options, cb) {
+        const req = (subsonicInstance.LAST_REQUEST = {
+          method,
+          url,
+          data,
+          headers: options.headers || {},
+          settings: options.settings || {},
+        });
+        if (auth) {
+          req.auth = auth;
+        }
+        if (host) {
+          req.host = host;
+        }
+        subsonicInstance.REQUESTS.push(req);
+        cb.call(this, null, {});
+      };
+    }
+
+    return subsonicInstance;
+  },
+
+  /**
+   * A utility where cleanup functions can be registered to be called post-spec.
+   * CleanupUtility will automatically register on the mocha afterEach hook,
+   * ensuring its called after each descendent-describe block.
+   */
+  CleanupUtility: (() => {
+    CleanupUtility.DEFAULT_TIMEOUT = 20000;
+
+    function CleanupUtility(timeout) {
+      const self = this;
+      this._cleanupFns = [];
+      this._subsonic = require('../lib/subsonic')(
+        process.env.USERNAME,
+        utils.getUserKey(),
+        process.env.SALT,
+        process.env.HOST
+      );
+      afterEach(function(done) {
+        this.timeout(timeout || CleanupUtility.DEFAULT_TIMEOUT);
+        return self.doCleanup(done);
+      });
+    }
+
+    CleanupUtility.prototype = {
+      doCleanup(done) {
+        const cleanups = this._cleanupFns;
+        const total = cleanups.length;
+        let completed = 0;
+        let fn;
+        while ((fn = cleanups.shift())) {
+          const promise = fn.call(this);
+          if (!promise || !promise.then) {
+            throw new Error(
+              'CleanupUtility expects cleanup functions to return promises!'
+            );
+          }
+          promise.then(
+            () => {
+              // cleanup successful
+              completed += 1;
+              if (completed === total) {
+                done();
+              }
+            },
+            (err) => {
+              // not successful
+              throw err;
+            }
+          );
+        }
+        if (total === 0) {
+          done();
+        }
+      },
+      add(fn) {
+        this._cleanupFns.push(fn);
+      },
+      //   deleteCustomer(custId) {
+      //     this.add(function() {
+      //       return this._stripe.customers.del(custId);
+      //     });
+      //   },
+      //   deletePlan(pId) {
+      //     this.add(function() {
+      //       return this._stripe.plans.del(pId);
+      //     });
+      //   },
+      //   deleteCoupon(cId) {
+      //     this.add(function() {
+      //       return this._stripe.coupons.del(cId);
+      //     });
+      //   },
+      //   deleteInvoiceItem(iiId) {
+      //     this.add(function() {
+      //       return this._stripe.invoiceItems.del(iiId);
+      //     });
+      //  },
+    };
+
+    return CleanupUtility;
+  })(),
+
+  /**
+   * Get a random string for test Object creation
+   */
+  getRandomString: () => {
+    return Math.random()
+      .toString(36)
+      .slice(2);
+  },
+
+  envSupportsForAwait: () => {
+    return typeof Symbol !== 'undefined' && Symbol.asyncIterator;
+  },
+
+  envSupportsAwait: () => {
+    try {
+      eval('(async function() {})'); // eslint-disable-line no-eval
+      return true;
+    } catch (err) {
+      return false;
+    }
+  },
+});
